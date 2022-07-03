16393fe7174b9aaf446924942f7a90bd695e15c1
Isaías <isaiasherreroflorensa@gmail.com>
2022-07-03 23:53:04 +0200

    Commit 1

---
+++ src/README.md
@@ -0,0 +1,3 @@
+# Backend-i-0ab8af2e8e3464134
+
+Código de la instancia de i-0ab8af2e8e3464134 (Servidor GraphQL)
---
+++ src/database.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+mongoose
+  .connect("mongodb://localhost/graphqlreactdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((db) => console.log("Db is connected"))
+  .catch((err) => console.log(err));
